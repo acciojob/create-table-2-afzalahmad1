e7c89b1cba7a224a6c275c7c64afe6f57c40686c
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-17 15:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function createTable() {
     //Write your code here
 	let rn = parseInt(prompt("Input number of rows"))
 	let cn = parseInt(prompt("Input number of columns"))
-	for(let i=0;i<rn;i++){
-		let row = table.insertRow(i)
-		for(let j=0j<cn;j++){
-			let col = row.insertCell(j)
-			col.innerText = `Row-${i} Column-${j}`;
-		}
-	}
+	for (let i = 0; i < rn; i++) {
+    const row = table.insertRow(i);
+    for (let j = 0; j < cn; j++) {
+      const cell = row.insertCell(j);
+      cell.innerText = `Row-${i} Column-${j}`;
+    }
+  }
 }
